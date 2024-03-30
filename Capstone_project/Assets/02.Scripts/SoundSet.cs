d713ba2c7c17b08b57fd4e2adcad5e79b488c65e
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-30 16:51:08 +0900

    Page Update
    2024.03.30/ Page Size and SoundBar modified

---
+++ Capstone_project/Assets/02.Scripts/SoundSet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSet : MonoBehaviour
+{
+    public void setVolume(float volume)
+    {
+        Debug.Log(volume);    
+    }
+}
