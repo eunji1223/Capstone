bdc423767800e2b0d6ef0de680a1ebe9a8587081
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-05-01 17:10:01 +0900

    정리

---
+++ Capstone_project/Assets/02.Scripts/ButtonManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] buttons;
+    [SerializeField]
+    private GameObject level;
+    [SerializeField]
+    private GameObject option;
+    
+
+    void Start(){
+        option.SetActive(false);
+        level.SetActive(false);
+        foreach(Button button in buttons){
+            button.onClick.AddListener(OnChooseModeClick);
+        }
+    }
+
+    void OnChooseModeClick(){
+        Button clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+
+        RectTransform buttonRectTransform = clickedButton.GetComponent<RectTransform>();
+        RectTransform levelRectTransform = level.GetComponent<RectTransform>();
+        levelRectTransform.anchoredPosition = new Vector3 (buttonRectTransform.anchoredPosition.x, buttonRectTransform.anchoredPosition.y - 90);
+        level.SetActive(true);
+        
+    }
+
+    public void ClickOptionButton(){
+        option.SetActive(true);
+    }
+
+    public void ClickCloseButton(){
+        option.SetActive(false);
+    }
+}
